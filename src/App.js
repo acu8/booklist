22cc1f6f4adb80ce5a2aa7fd5fb3e9aeda3a13fc
acu8 <84504738+acu8@users.noreply.github.com>
2023-07-04 21:39:22 +0900

    create repo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import BookCreate from './components/BookCreate';
+
+export default function App() {
+    const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+        console.log('Need to add book with', title);
+    };
+
+  return (
+    <div>
+        <BookCreate onCreate={createBook} />
+    </div>
+  )
+}
