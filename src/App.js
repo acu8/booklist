2c2a26c491aeca5efd6aacc4609a6f4613658dfe
acu8 <ewatanabe786@gmail.com>
2023-07-08 22:05:53 +0900

    edit and delete

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import BookList from './components/BookList'
 export default function App() {
     const [books, setBooks] = useState([]);
 
+    const editBookById = (id, newTitle) => {
+      const updatedBooks = books.map((book) => {
+        if(book.id === id) {
+          return {...book, title: newTitle}
+        }
+        return book;
+      });
+      setBooks(updatedBooks);
+
+    }
+
     const deleteBookById = (id) => {
       const updatedBooks = books.filter((book) => {
         return book.id !== id;
@@ -16,7 +27,7 @@ export default function App() {
         const updateBooks = [
           ...books,
           { 
-            id: Math.round(Math.random * 9999), 
+            id: Math.round(Math.random() * 9999), 
             title }
         ];
         setBooks(updateBooks);
@@ -24,7 +35,7 @@ export default function App() {
 
   return (
     <div className='app'>
-        <BookList books={books} onDelete={deleteBookById} />
+        <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
         <BookCreate onCreate={createBook} />
     </div>
   )
--- src/components/Bookedit.js
+++ src/components/Bookedit.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Bookedit() {
+export default function Bookedit({ book, onSubmit }) {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    onSubmit(book.id, title);
+  }
+
   return (
-    <div>Bookedit</div>
+    <form onSubmit={handleSubmit} className='book-edit'>
+      <label>Title</label>
+      <input className='input' value={title} onChange={handleChange} />
+      <button className='button is-primary'>
+        Save
+      </button>
+    </form>
   )
 }
